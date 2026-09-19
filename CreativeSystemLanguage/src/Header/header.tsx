bcd592a7657061f8d29d2f1732129ef08ac69631
Jeremy <siejeremiah@gmail.com>
2026-09-20 06:35:25 +0700

    Added Assets Headercontent (about=null) succesfullyadded node modules and maintsx and other packages

---
+++ CreativeSystemLanguage/src/Header/header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <div className="HeaderContent">
+       <header>
+         <nav>
+            <a href="home.html">Home</a>
+            <a href="about.html">About CSL</a>
+            <a href="docs.html">CSL Docs</a>
+            <a href="devs.html">CSL Developers</a>
+         </nav>
+       </header>
+    </div>     
+  );
+}
+export default Header;
